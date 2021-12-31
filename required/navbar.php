9a0c3b470e8e5cee1bb3007e2c895002d6d61753
Saroj Prasad <prasad.hunny03@gmail.com>
2021-12-31 17:41:35 +0530

    Homepage initalized

---
+++ required/navbar.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-light bg-none">
+    <div class="container">
+        <a class="navbar-brand" href="#" style="color: #093466;">Yellow<span class="fw-bold" style="color: #f0c21a;">duckies</span></a>
+        <a class="btn" type="button" style="color: #093466;" data-bs-toggle="offcanvas" data-bs-target="#menuButton" aria-controls="menuButton">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+            </svg>
+        </a>
+    </div>
+</nav>
+
+<div class="offcanvas offcanvas-end" tabindex="-1" id="menuButton" aria-labelledby="menuButtonLabel">
+    <div class="offcanvas-header">
+        <h5 id="menuButtonLabel">Offcanvas right</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+        ...
+    </div>
+</div>
